f691086f9d9026326d1b6adad2ec07f2aef5e646
junil-hwang <tjsdlf4261@naver.com>
2019-03-22 11:15:58 +0900

    root path 생성 완료

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import model from 'middleware/Model'
 
 class Menu extends Component {
   render() {
+    const category: number = model.getCategory()
+    const rootActive: string = category === -1 ? 'active' : ''
     return (
       <nav className="site-menu">
         <h2><a href="#">Category</a></h2>
         <ul>
-          <li><a href="#">ALL</a></li>
+          <li><a href="#" className={rootActive}>ALL</a></li>
+          {
+            model.getCategoryList().map((v, k) => <li key={k}><a href="#" className={category === k ? 'active' : ''}>{v}</a></li>)
+          }
         </ul>
       </nav>
     );
